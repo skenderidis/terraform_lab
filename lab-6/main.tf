afefec49deeb5de48aad37107c31ef4123474edf
Kostas Skenderidis <skenderidis@gmail.com>
2022-04-10 19:58:03 +0400

    lab-6

--- lab-6/main.tf
+++
@@ -1,8 +0,0 @@
-resource "local_file" "pass_each" { 
-  for_each = {
-    file1 = "this is file 1"
-    file2 = "this is file 2"
-  }
-  filename = "/var/tmp/each_${each.key}.txt"
-  content = each.value
-}
